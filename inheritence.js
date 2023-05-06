19fbb7c65bbcf471bf05ea1534067abd637784c2
Ador-25 <abtahee.ador25@gmail.com>
2023-05-07 00:01:18 +0600

    prototype inheritence

--- inheritence.js
+++ inheritence.js
@@ -7,3 +7,12 @@ function Book(title,author,year){
 Book.prototype.getSummary = function(){
     return  this.title+' was written by '+this.author+' in '+this.year
 }
+
+function Magazine(title,author,year,month){
+    Book.call(this,title,author,year)
+    this.month = month
+}
+Magazine.prototype = Object.create(Book.prototype)
+
+const maganize =  new Magazine('CS 102','Jon','2008','Jan')
+console.log(maganize.getSummary())
